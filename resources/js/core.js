22a94eb4f330de13de2407ab09c6b8927b6e1059
wmpaul <willem.m.paul@gmail.com>
2019-10-22 16:24:21 -0500

    Make FAQ items collapsable

--- resources/js/core.js
+++ resources/js/core.js
@@ -1,7 +1,23 @@
 function onNavbarMenuClick() {
-    var navbar = document.getElementById("navbar");
-    if (!navbar.classList.contains("responsive"))
-        navbar.classList.add("responsive");
-    else
-        navbar.classList.remove("responsive");
+	var navbar = document.getElementById("navbar");
+
+	if (!navbar.classList.contains("responsive"))
+		navbar.classList.add("responsive");
+	else
+		navbar.classList.remove("responsive");
+}
+
+function onCollapsibleItemClick(element) {
+	var collapsibleItems = document.getElementsByClassName("collapsible-item");
+
+	for (let i = 0; i < collapsibleItems.length; i++) {
+		const item = collapsibleItems[i];
+		if (item != element && item.classList.contains("expanded"))
+			item.classList.remove("expanded");
+	}
+
+	if (!element.classList.contains("expanded"))
+		element.classList.add("expanded");
+	else
+		element.classList.remove("expanded");
 }
